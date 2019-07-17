4f779f84e37c07f8a83b4e6356ed7866855e897c
Lavanya Subramanian <lali.subramanian2005@gmail.com>
2019-07-17 13:02:02 -0700

    second version --> with app icon

--- MySwiftApp2.0/ViewController.swift
+++ MySwiftApp2.0/ViewController.swift
@@ -1,20 +1,34 @@
 //
 //  ViewController.swift
-//  MySwiftApp2.0
+//  MyFirstSwiftApp
 //
-//  Created by 23LavanyaSubramanian on 7/17/19.
+//  Created by 23LavanyaSubramanian on 7/15/19.
 //  Copyright © 2019 Lavanya Subramanian. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    var showFirstImage: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    @IBAction func buttonClick(_ sender: Any) {
+        if(showFirstImage == false) {
+            imageView1.image = UIImage(named: "Eevee")
+            showFirstImage = true
+        }
+        else {
+            imageView1.image = UIImage(named: "selected_star")
+            showFirstImage = false
+            
+        }
+        
+    }
 }
 
